9cd559dd816c1490dbe64bb28d2f6c4bf3dfa291
cbsawyer <cbsawyer>
2023-01-17 12:04:51 -0700

    Markdown rendering?

---
+++ Notes/Week 2 notes.R
@@ -0,0 +1,24 @@
+# Class notes jan 17, 2023
+
+#Week 2 begins now :) 
+#Hashtag means everything to the right on that line is for humans
+#It won't be read as R code
+TRUE # is a special term, means yes or 1
+FALSE #is special as well, means no, or 0
+
+TAB #helps autocomplete
+ #,comma separates argument to a function
+#Find out what argument, function,
+# # Ctrl+Shift+C makes it all purple/hashtagged
+# # Poop
+#ignore.case = TRUE to find a file now knowing about caps
+#"Recursive"?
+#Ctrl + Alt + B, repeat all lines of code
+#Broom above environment clears it all out
+#Dollar sign $ is for column names
+#Wingspan is a DATA FRAME (Got rows and columns) 
+# Data frame: 2 dimensional, has rows and columns
+# vector: 1-dimensional, numbers in order (1 column/1 row)
+#plot, assign value to path, 
+
+# rmarkdown::render("foo.Rmd"). --> Ask about markdown rendering?
